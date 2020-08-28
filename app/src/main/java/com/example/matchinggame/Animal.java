78bca3ba4bbbf42d3a7ad22aa0fc4fcbd9f763d4
Mohamed Tawfik <motawfik1998@gmail.com>
2020-08-28 13:28:50 +0200

    Preserve state when screen rotates

--- app/src/main/java/com/example/matchinggame/Animal.java
+++ app/src/main/java/com/example/matchinggame/Animal.java
@@ -1,15 +1,34 @@
 package com.example.matchinggame;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.Nullable;
 
-public class Animal {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Animal implements Parcelable {
     private int image;
     private int sound;
+    private static ArrayList<Animal> animals;
 
     public Animal(int image, int sound) {
         this.image = image;
         this.sound = sound;
     }
+
+    public static final Creator<Animal> CREATOR = new Creator<Animal>() {
+        @Override
+        public Animal createFromParcel(Parcel in) {
+            return new Animal(in);
+        }
+
+        @Override
+        public Animal[] newArray(int size) {
+            return new Animal[size];
+        }
+    };
 
     public int getImage() {
         return image;
@@ -23,7 +42,41 @@ public class Animal {
     public boolean equals(@Nullable Object obj) {
         if (obj == null)
             return false;
+        if (!(obj instanceof Animal))
+            return false;
         Animal animal = (Animal) obj;
         return image == animal.image && sound == animal.sound;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(image);
+        parcel.writeInt(sound);
+    }
+
+    private Animal(Parcel in) {
+        image = in.readInt();
+        sound = in.readInt();
+    }
+
+    public static ArrayList<Animal> getAnimals() {
+        if (animals == null) {
+            animals = new ArrayList<>();
+            animals.add(new Animal(R.drawable.dog, R.raw.chasdog));
+            animals.add(new Animal(R.drawable.dog, R.raw.chasdog));
+            animals.add(new Animal(R.drawable.lion, R.raw.lion4));
+            animals.add(new Animal(R.drawable.lion, R.raw.lion4));
+            animals.add(new Animal(R.drawable.monkey, R.raw.monkeypatas));
+            animals.add(new Animal(R.drawable.monkey, R.raw.monkeypatas));
+            animals.add(new Animal(R.drawable.elephant, R.raw.elephant9));
+            animals.add(new Animal(R.drawable.elephant, R.raw.elephant9));
+            Collections.shuffle(animals);
+        }
+        return animals;
     }
 }
--- app/src/main/java/com/example/matchinggame/MainActivity.java
+++ app/src/main/java/com/example/matchinggame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.matchinggame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
@@ -11,26 +12,19 @@ import android.widget.ImageButton;
 
 import com.example.matchinggame.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    List<Animal> animals = Arrays.asList(
-            new Animal(R.drawable.dog, R.raw.chasdog),
-            new Animal(R.drawable.dog, R.raw.chasdog),
-            new Animal(R.drawable.lion, R.raw.lion4),
-            new Animal(R.drawable.lion, R.raw.lion4),
-            new Animal(R.drawable.monkey, R.raw.monkeypatas),
-            new Animal(R.drawable.monkey, R.raw.monkeypatas),
-            new Animal(R.drawable.elephant, R.raw.elephant9),
-            new Animal(R.drawable.elephant, R.raw.elephant9)
-    );
+    ArrayList<Animal> animals;
     ImageButton[] buttons = new ImageButton[8];
+    int[] displayImages = new int[8];
     int firstAnimalIndex = -1, secondAnimalIndex = -1;
     MediaPlayer player;
-    boolean handlerRunning = false, match = false;
+    boolean handlerRunning = false, match = false, shuffled = false;
     Handler handler = new Handler();
 
     @Override
@@ -41,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        Collections.shuffle(animals);
+        Arrays.fill(displayImages, R.drawable.card);
+        animals = Animal.getAnimals();
+
         for (int i = 0; i < 8; i++) {
             buttons[i] = view.findViewWithTag(String.valueOf(i));
             buttons[i].setMaxWidth(buttons[i].getWidth());
@@ -69,43 +65,87 @@ public class MainActivity extends AppCompatActivity {
         } else if (secondAnimalIndex == -1) {
             secondAnimalIndex = value;
         }
-        buttons[value].setImageResource(animals.get(value).getImage());
+
+        changeImageButton(value, animals.get(value).getImage());
         player = MediaPlayer.create(MainActivity.this, animals.get(value).getSound());
         player.start();
 
         if (secondAnimalIndex != -1) {
             handlerRunning = true;
             match = animals.get(firstAnimalIndex).equals(animals.get(secondAnimalIndex));
-            delay();
+            delay(3000);
         }
     }
 
-    void delay() {
+    void delay(int time) {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 handlerCode();
             }
-        }, 3000);
+        }, time);
     }
 
     void handlerCode() {
         if (match) {
-            buttons[firstAnimalIndex].setVisibility(View.INVISIBLE);
-            buttons[secondAnimalIndex].setVisibility(View.INVISIBLE);
+            changeImageButton(firstAnimalIndex, 0);
+            changeImageButton(secondAnimalIndex, 0);
         } else {
-            buttons[firstAnimalIndex].setImageResource(R.drawable.card);
-            buttons[secondAnimalIndex].setImageResource(R.drawable.card);
+            changeImageButton(firstAnimalIndex, R.drawable.card);
+            changeImageButton(secondAnimalIndex, R.drawable.card);
         }
         firstAnimalIndex = -1;
         secondAnimalIndex = -1;
         handlerRunning = false;
     }
 
+    void changeImageButton(int index, int image) {
+        displayImages[index] = image;
+        if (image == 0)
+            buttons[index].setVisibility(View.INVISIBLE);
+        else
+            buttons[index].setImageResource(image);
+    }
+
+
     @Override
     protected void onDestroy() {
         if (player != null)
             player.stop();
         super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray("displayImages", displayImages);
+
+        outState.putInt("firstAnimalIndex", firstAnimalIndex);
+        outState.putInt("secondAnimalIndex", secondAnimalIndex);
+        outState.putBoolean("handlerRunning", handlerRunning);
+        outState.putBoolean("match", match);
+        outState.putBoolean("shuffled", shuffled);
+        outState.putParcelableArrayList("animals", animals);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        displayImages = savedInstanceState.getIntArray("displayImages");
+        firstAnimalIndex = savedInstanceState.getInt("firstAnimalIndex");
+        secondAnimalIndex = savedInstanceState.getInt("secondAnimalIndex");
+        handlerRunning = savedInstanceState.getBoolean("handlerRunning");
+        match = savedInstanceState.getBoolean("match");
+        animals = savedInstanceState.getParcelableArrayList("animals");
+
+        if (displayImages != null)
+            for (int i = 0; i < displayImages.length; i++) {
+                if (displayImages[i] == 0)
+                    buttons[i].setVisibility(View.INVISIBLE);
+                else
+                    buttons[i].setImageResource(displayImages[i]);
+            }
+        if (handlerRunning)
+            delay(1000);
     }
 }
